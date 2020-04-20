4d4aabac4dfea9d56b71ef18ceea1f603963761f
kr123-ui <62220326+kr123-ui@users.noreply.github.com>
2020-04-20 21:46:33 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+function darktheme() {
+    document.querySelector('body').classList.toggle("dark-mode");
+    document.querySelectorAll('.box')[0].classList.toggle("dark-box");
+    document.querySelectorAll('.box')[1].classList.toggle("dark-box");
+    document.querySelectorAll('.box')[2].classList.toggle("dark-box");
+    document.querySelectorAll('.box')[3].classList.toggle("dark-box");
+    document.querySelectorAll('.box')[4].classList.toggle("dark-box");
+    document.querySelectorAll('.box')[5].classList.toggle("dark-box");
+    document.querySelectorAll('.box')[6].classList.toggle("dark-box");
+    document.querySelectorAll('.box')[7].classList.toggle("dark-box");
+    document.querySelectorAll('.box')[8].classList.toggle("dark-box");
+    document.querySelectorAll('.box')[9].classList.toggle("dark-box");
+    document.querySelectorAll('.box')[10].classList.toggle("dark-box");
+    document.querySelectorAll('.box')[11].classList.toggle("dark-box");
+  }
